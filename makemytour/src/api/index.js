cbb99431d29098063e2d0ddda9fdb523582c2251
ssahuji910@gmail.com <ssahuji910@gmail.com>
2026-08-01 22:05:41 +0530

    Added backend API url

--- makemytour/src/api/index.js
+++ makemytour/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://make-my-trip-clone-springboot-2-0p35.onrender.com";
 export const login = async (email, password) => {
   try {
     const url = `${BACKEND_URL}/user/login?email=${email}&password=${password}`;
